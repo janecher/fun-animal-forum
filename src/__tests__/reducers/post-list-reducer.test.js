e85c83077d569ef840c8ac6ec78ab4da3c59d510
Kate Skorija <kateskorija@gmail.com>
2020-09-15 14:12:42 -0700

    create failing and passing tests for adding a post to post list
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/__tests__/reducers/post-list-reducer.test.js
+++ src/__tests__/reducers/post-list-reducer.test.js
@@ -1,7 +1,45 @@
 import postListReducer from '../../reducers/post-list-reducer';
 
 describe('postListReducer', () => {
+
+  let action;
+  const postData = {
+    title: "Mysterious Eels",
+    username: "kate",
+    message: "Scientists still have not observed eels breeding in their natural habitat -- the deepest trenches in the ocean.",
+    timestamp: new Date(),
+    upvotes: 0,
+    downvotes: 0,
+    id: 1
+  };
+
   test('Should return default state if there is not action type passed into the reducer', () => {
     expect(postListReducer({}, { type: null })).toEqual({});
   });
+
+  test('Should successfully add new post data to masterPostList', () => {
+    const { title, username, message, timestamp, upvotes, downvotes, id } = postData;
+    action = {
+      type: 'ADD_POST',
+      title: title,
+      username: username,
+      message: message,
+      timestamp: timestamp,
+      upvotes: upvotes,
+      downvotes: downvotes,
+      id: id
+    };
+    expect(postListReducer({}, action)).toEqual({
+      [id] : {
+        title: title,
+        username: username,
+        message: message,
+        timestamp: timestamp,
+        upvotes: upvotes,
+        downvotes: downvotes,
+        id: id
+      }
+    });
+  });
+  
 });
--- src/reducers/post-list-reducer.js
+++ src/reducers/post-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  const { title, username, message, timestamp, upvotes, downvotes, id } = action;
+  switch (action.type) {
+    case 'ADD_POST':
+      return Object.assign({}, state, {
+        [id]: {
+          title: title,
+          username: username,
+          message: message,
+          timestamp: timestamp,
+          upvotes: upvotes,
+          downvotes: downvotes,
+          id: id
+        }
+      });
+    default:
+      return state;
+  }
 };
