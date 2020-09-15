808a8136eb914d2d3bf33d3753b2e0dfff58d9c5
Kate Skorija <kateskorija@gmail.com>
2020-09-15 11:17:46 -0700

    create post list component
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>
    Co-authored-by: Evgeniya Chernaya <evgenya.chernaya@gmail.com>

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,12 +2,15 @@ import React from "react";
 import PropTypes from "prop-types";
 
 function Post(props) {
+
   return (
     <React.Fragment>
       <h3>{props.title}</h3>
       <h3>{props.message}</h3>
       <h3>{props.timestamp}</h3>
       <h3>{props.username}</h3>
+      <button onClick={() => props.whenClickingUpVote(props.id)}>Upvote</button>
+      <button onClick={() => props.whenClickingDownVote(props.id)}>Downvote</button>
       <hr />
     </React.Fragment>
   );
@@ -15,7 +18,26 @@ function Post(props) {
 
 Post.propTypes = {
   title: PropTypes.string,
-  message: PropTypes.string, 
+  message: PropTypes.string,
   timestamp: PropTypes.string,
-
+  username: PropTypes.string,
+  whenClickingUpVote: PropTypes.func,
+  whenClickingDownVote: PropTypes.func
 };
+
+export default Post;
+
+  //const { post, onClickingUpVote, onClickingDownVote } = props;
+
+  //   return (
+  //     <React.Fragment>
+  //       <h3>{post.title}</h3>
+  //       <h3>{post.message}</h3>
+  //       <h3>{post.timestamp}</h3>
+  //       <h3>{post.username}</h3>
+  //       <button onClick={() => onClickingUpVote(post.id)}>Upvote</button>
+  //       <button onClick={() => onClickingDownVote(post.id)}>Downvote</button>
+  //       <hr />
+  //     </React.Fragment>
+  //   );
+  // }
---
+++ src/components/PostList.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+import Post from "./Post";
+
+function PostList(props) {
+  return (
+    <React.Fragment>
+      <hr />
+      {Object.values(props.postList).map((post) => {
+        return <Post
+          //post={post}
+          title={post.title}
+          message={post.message}
+          timestamp={post.timestamp}
+          username={post.username}
+          id={post.id}
+          key={post.id}
+          whenClickingUpVote={props.onClickingUpVote}
+          whenClickingDownVote={props.onClickingDownVote}
+        />
+      })}
+    </React.Fragment>
+  );
+}
+
+PostList.propTypes = {
+  postList: PropTypes.object,
+  onClickingUpVote: PropTypes.func,
+  onClickingDownVote: PropTypes.func
+}
+
+export default PostList;
